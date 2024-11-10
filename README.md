06a37e9578f8f022232d4583ffdc10a6b284ab6a
jsnam <skawotkd12@naver.com>
2024-11-10 22:43:46 +0900

    jsnam

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# H-J-Chat-bot
-chat ai를 기반으로 만든 프로젝트
+# hj-chatbot
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <div class="search-content">
+        <h3>🤖 무엇을 도와드릴까요?</h3>
+        <div class="input-wrapper">
+          <input type="text" placeholder="메시지를 입력하세요..." />
+          <button>전송</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import "@/assets/css/global.scss";
+</script>
+
+<style>
+</style>
